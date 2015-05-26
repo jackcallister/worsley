d25b0eb6d0db6d6a82846aa98482f7932f9c377e
jarsbe <jarsbe@gmail.com>
2015-05-26 21:23:20 +0100

    Added store tests.

---
+++ src/__tests__/store-tests.js
@@ -0,0 +1,56 @@
+'use strict';
+
+import { Dispatcher } from 'flux';
+import { Store } from '../worsley';
+import assert from 'assert';
+import sinon from 'sinon';
+
+describe('Store', () => {
+
+  const dispatcher = new Dispatcher();
+  const flux = { dispatcher: dispatcher };
+
+  class TestStore extends Store {
+    constructor() {
+      super(flux);
+    }
+  }
+
+  describe('#registerActionHandler', () => {
+    const store = new TestStore();
+    const method = sinon.spy();
+    const actionConstant = 'testConstant';
+
+    store.registerActionHandler(actionConstant, method);
+
+    it('calls the handler', () => {
+      dispatcher.dispatch({ type: actionConstant, data: null });
+      sinon.assert.calledOnce(method);
+    });
+  });
+
+  describe('#setState', () => {
+    const store = new TestStore();
+    store.state = { data: 'data' };
+    const expectedState = {
+      data: 'data',
+      newData: 'newData'
+    }
+
+    it('non-destructively assigns state', () => {
+      store.setState({
+        newData: 'newData'
+      });
+
+      assert.deepEqual(expectedState, store.state);
+    });
+
+    it('emits a change event', () => {
+      const method = sinon.spy();
+      store.emitChange = method;
+      store.setState({});
+
+      sinon.assert.calledOnce(method);
+    });
+  });
+});
