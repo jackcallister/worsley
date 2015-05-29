3993cfde97fd2cc0f1d1bc189bbb95158132af10
Jack Callister <jack.callister@quillcontent.com>
2015-05-29 08:46:33 +0100

    Test unloading.

--- src/__tests__/worsley-tests.js
+++ src/__tests__/worsley-tests.js
@@ -4,41 +4,44 @@ import { Worsley, Store } from '../worsley';
 import assert from 'assert';
 import sinon from 'sinon';
 
-class TestStore extends Store {
-  constructor(flux) {
-    super(flux);
-    this.state = {
-      foo: 'bar'
-    };
-  }
-}
-
-class StoreWorsley extends Worsley {
-  constructor() {
-    super();
-    this.stores.TestStore = new TestStore(this);
-  }
-}
-
-class EmptyWorsley extends Worsley {}
 
 describe('Worsley', () => {
-  const emptyWorsley = new EmptyWorsley();
 
-  it('initializes with actions', () => {
-    assert.deepEqual(emptyWorsley.actions, {});
-  });
+  const initialState = { foo: 'bar' };
 
-  it('initializes with stores', () => {
-    assert.deepEqual(emptyWorsley.stores, {});
-  });
+  class TestStore extends Store {
+    constructor(flux) {
+      super(flux);
+      this.setInitialState(initialState);
+    }
+  }
 
-  it('initializes with a dispatcher', () => {
-    assert.notEqual(emptyWorsley.dispatcher, undefined);
+  class TestWorsley extends Worsley {
+    constructor() {
+      super();
+      this.stores.TestStore = new TestStore(this);
+    }
+  }
+
+  describe('#constructor', () => {
+    class EmptyWorsley extends Worsley {}
+    const emptyWorsley = new EmptyWorsley();
+
+    it('initializes with actions', () => {
+      assert.deepEqual(emptyWorsley.actions, {});
+    });
+
+    it('initializes with stores', () => {
+      assert.deepEqual(emptyWorsley.stores, {});
+    });
+
+    it('initializes with a dispatcher', () => {
+      assert.notEqual(emptyWorsley.dispatcher, undefined);
+    });
   });
 
   describe('#load', () => {
-    let storeWorsley = new StoreWorsley();
+    let testWorsley = new TestWorsley();
     const payload = {
       TestStore: {
         foo: 'baz'
@@ -46,22 +49,22 @@ describe('Worsley', () => {
     };
 
     it('loads stores with data', () => {
-      storeWorsley.load(payload);
-      assert.deepEqual(storeWorsley.stores.TestStore.state, payload.TestStore);
+      testWorsley.load(payload);
+      assert.deepEqual(testWorsley.stores.TestStore.state, payload.TestStore);
     });
   });
 
   describe('#unload', () => {
-    let storeWorsley = new StoreWorsley();
+    let testWorsley = new TestWorsley();
 
-    xit('resets store data', () => {
-      storeWorsley.unload();
-      assert.deepEqual(storeWorsley.stores.TestStore.state, { foo: 'bar' });
+    it('resets store data', () => {
+      testWorsley.unload();
+      assert.deepEqual(testWorsley.stores.TestStore.state, initialState);
     });
   });
 
   describe('#backup', () => {
-    let storeWorsley = new StoreWorsley();
+    let testWorsley = new TestWorsley();
 
     const expectedPayload = {
       TestStore: {
@@ -70,7 +73,7 @@ describe('Worsley', () => {
     };
 
     it('returns the current payload', () => {
-      const payload = storeWorsley.backup();
+      const payload = testWorsley.backup();
       assert.deepEqual(expectedPayload, payload);
     });
   });
